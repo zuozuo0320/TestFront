12a5fb3f4bca3cc5bc613f8d2f30e82d4753c46d
zuozhifu <zuozhifu@baimaohui.net>
2026-03-07 23:24:57 +0800

    style(front): enable zh-cn locale and unify pagination in monochrome theme

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(createPinia())
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: (zhCn as any).default ?? (zhCn as any) })
 app.mount('#app')
