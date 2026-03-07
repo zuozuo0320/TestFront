3cf05b4e6f887010f2703a9b168a52b1d61c7c33
zuozhifu <zuozhifu@baimaohui.net>
2026-03-07 23:35:47 +0800

    style(front): refine advanced filter/query/reset toolbar in unified monochrome look

--- src/App.vue
+++ src/App.vue
@@ -525,27 +525,27 @@ onMounted(async () => {
                   <div class="left">
                     <el-button type="primary" @click="openCreate">新建</el-button>
                   </div>
-                  <div class="right">
-                    <el-input v-model="keyword" placeholder="通过 ID/名称/标签搜索" style="width: 220px" clearable @keyup.enter="onSearch" />
-                    <el-select v-model="levelFilter" style="width: 100px" placeholder="等级" clearable>
+                  <div class="right filter-bar">
+                    <el-input v-model="keyword" class="query-input" placeholder="通过 ID/名称/标签搜索" clearable @keyup.enter="onSearch" />
+                    <el-select v-model="levelFilter" class="filter-select level" placeholder="等级" clearable>
                       <el-option label="P0" value="P0" />
                       <el-option label="P1" value="P1" />
                       <el-option label="P2" value="P2" />
                     </el-select>
-                    <el-select v-model="reviewFilter" style="width: 120px" placeholder="评审" clearable>
+                    <el-select v-model="reviewFilter" class="filter-select review" placeholder="评审" clearable>
                       <el-option label="未评审" value="未评审" />
                       <el-option label="已通过" value="已通过" />
                       <el-option label="不通过" value="不通过" />
                       <el-option label="重新提审" value="重新提审" />
                     </el-select>
-                    <el-select v-model="execFilter" style="width: 120px" placeholder="执行" clearable>
+                    <el-select v-model="execFilter" class="filter-select exec" placeholder="执行" clearable>
                       <el-option label="未执行" value="未执行" />
                       <el-option label="成功" value="成功" />
                       <el-option label="失败" value="失败" />
                       <el-option label="阻塞" value="阻塞" />
                     </el-select>
-                    <el-button @click="onSearch">查询</el-button>
-                    <el-button @click="onResetSearch">重置</el-button>
+                    <el-button class="toolbar-btn btn-query" @click="onSearch">查询</el-button>
+                    <el-button class="toolbar-btn btn-reset" @click="onResetSearch">重置</el-button>
                   </div>
                 </div>
 
